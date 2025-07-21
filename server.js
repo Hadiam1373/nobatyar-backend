db4852a075bc8a4475378f1fe411c8f7b8993390
hadi moradi <h.moradi@gmail.com>
2025-07-21 20:20:39 +0330

    بهبود فرمت کد در فایل server.js با استفاده از فرمت جدید برای اتصال به MongoDB و افزودن فاصله‌های مناسب.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const app = require("./src/app");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(process.env.PORT || 5000, () =>
       console.log("🚀 Server running on port " + process.env.PORT)
